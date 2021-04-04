6ddebb257043b41de1e1cef0f4cb3d2d53035d2b
Pedro Conde <pepconde.1993@gmail.com>
2021-04-04 22:11:27 +0100

    fixed css

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,18 +1,18 @@
 import "../assets/scss/main.scss";
 import { main } from "../../conde/demos/shadows/shadows-real";
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
     useEffect(() => {
-        main();
-      });
+      main();
+    });
 
     return (
-        <main className="main">
-          <canvas className="main-canvas"></canvas>
-        </main>
-      );
+      <main className="main">
+        <canvas className="main-canvas"></canvas>
+      </main>
+    );
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'));
