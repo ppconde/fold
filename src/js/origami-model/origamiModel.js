bcfaacdb7223eeea9ec876a0b94cffb539b00f42
Pedro Conde <pepconde.1993@gmail.com>
2021-04-18 16:05:14 +0100

    Changed material back to a shadow castable material

--- src/js/origami-model/origamiModel.js
+++ src/js/origami-model/origamiModel.js
@@ -4,14 +4,14 @@ export class OrigamiModel {
 	constructor(scene) {
 		// Temporary geo and material
 		this.geometry = new THREE.BoxBufferGeometry(2, 2, 2);
-		this.material = new THREE.MeshBasicMaterial({ color: new THREE.Color("rgb(100,0,0)") });
+		this.material = new THREE.MeshStandardMaterial({ color: new THREE.Color("rgb(100,0,0)") });
 		this.mesh = new THREE.Mesh(this.geometry, this.material);
 		scene.add(this.mesh);
 	}
 
 	update = () => {
 		// Do something
-		const scale = Math.sin(Date.now()*0.001);
+		const scale = Math.sin(Date.now()*0.001) + 3;
 		this.mesh.scale.set(scale, scale, scale);
 	}
 }
