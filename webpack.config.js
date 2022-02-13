3470e5ae306f4745fff4c5ef8d141d21e713ba37
CondeP <pedro.conde@paddypowerbetfair.com>
2022-02-13 17:44:24 +0000

    Fixed babel incorrect scoping for variables

--- webpack.config.js
+++ webpack.config.js
@@ -45,10 +45,15 @@ module.exports = {
 				],
 			},
 			{
-				test: /\.(js|jsx)$/,
+				test: /\.m?js|jsx$/,
 				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			}
 		],
 	},
 	resolve: {
