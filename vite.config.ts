991503f301b463e635deb286fc9824d3d1caa3bd
Pedro Conde <pedro.conde@blip.pt>
2023-12-08 00:01:40 +0000

    More configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // This is not working when set to true because of problems exposing the server to the local network
+    host: false, // Open to local network and display URL
+    port: 5173,
+  },
+  build: {
+    outDir: "../dist", // Output in the dist/ folder
+    emptyOutDir: true, // Empty the folder first
+    sourcemap: true, // Add sourcemap
+  },
+});
