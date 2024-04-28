ac54d6badc1a41a52475ed30044ff04c02802b05
Diogo Duarte <dierduarte@gmail.com>
2024-04-28 17:49:12 +0100

    only show outline border on start

--- src/scene/models/line.ts
+++ src/scene/models/line.ts
@@ -2,14 +2,18 @@ import * as THREE from 'three';
 
 export class Outline extends THREE.Object3D {
   public geometry: THREE.BufferGeometry<THREE.NormalBufferAttributes>;
+  private width: number;
+  private height: number;
 
   public lineWidth: number;
 
   public vertices: THREE.Vector3[] = [];
 
-  constructor(geometry: THREE.BufferGeometry, lineWidth = 0.02) {
+  constructor(geometry: THREE.BufferGeometry, width: number, height: number, lineWidth: number = 0.02) {
     super();
     this.geometry = geometry;
+    this.width = width;
+    this.height = height;
     this.lineWidth = lineWidth;
 
     this.calculateVertices();
@@ -38,6 +42,13 @@ export class Outline extends THREE.Object3D {
       const nextVec = this.vertices[(((i + 1) % len) + len) % len];
 
       const lineVector = nextVec.clone().sub(vec);
+      const border =
+        lineVector.x != 0 && lineVector.x != this.width
+          ? lineVector.y != 0 && lineVector.y != this.height
+            ? false
+            : true
+          : true;
+
       const lineLength = lineVector.length();
       const normalizedVec = lineVector.clone().normalize();
       const halfLineLength = normalizedVec.clone().multiplyScalar(lineLength / 2.0);
@@ -56,7 +67,19 @@ export class Outline extends THREE.Object3D {
       pivot.lookAt(nextVec);
       line.rotation.x = Math.PI / 2.0;
 
+      pivot.visible = border;
       this.add(pivot);
+    }
+  }
+
+  public changeVisibility(visible: boolean, index: number = -1) {
+    // if no index if define, change the visilibity to all lines
+    if (index == -1) {
+      for (let index = 0; index < this.children.length; index++) {
+        this.children[index].visible = visible;
+      }
+    } else if (index >= 0 && index <= 2) {
+      this.children[index].visible = visible;
     }
   }
 }
